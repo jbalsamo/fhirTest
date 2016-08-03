aa21fd8c52ffa8d292c5ae47feaf59ee3af849ab
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2016-08-03 11:15:51 -0400

    updated js to update table

--- fhirtest.js
+++ fhirtest.js
@@ -9,3 +9,16 @@ fhir.get = function (url, callback) {
         callback(data.entry);
     });
 };
+
+da = {};
+
+da.proc = function (data) {
+    data.map(function (patient) {
+        $('#dt tbody').append("<tr><td>" + patient.resource.name.text + "</td><td>" + patient.resource.birthDate + "</td></tr>");
+    });
+};
+
+$(document).ready(function () {
+    var url = "https://fhir-open.sandboxcernerpowerchart.com/dstu2/d075cf8b-3261-481d-97e5-ba6c48d3b41f/Patient?name=Pond";
+    fhir.get(url, da.proc);
+});
